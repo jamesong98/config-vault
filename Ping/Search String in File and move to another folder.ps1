3940aaede11df9c6ccc7c86f2d1651e46ef9394e
james-1912 <james@silverliningsys.com>
2021-04-27 10:29:26 +0800

    add powershell to search string in file

---
+++ Ping/Search String in File and move to another folder.ps1
@@ -0,0 +1,19 @@
+﻿$Path = "D:\temp\hello\fail"
+$Text = "hi"
+$PathArray = @()
+$Results = "D:\temp\hello\filetomove.txt"
+
+
+# This code snippet gets all the files in $Path that end in ".txt".
+Get-ChildItem $Path -Filter "*.txt" |
+Where-Object { $_.Attributes -ne "Directory"} |
+ForEach-Object {
+If (Get-Content $_.FullName | Select-String -Pattern $Text) {
+$PathArray += $_.FullName
+
+}
+}
+Write-Host
+$PathArray | ForEach-Object {$_}  >> $Results
+
+Move-Item -Path $(Get-Content $Results) -Destination "D:\temp\hello\staging"
